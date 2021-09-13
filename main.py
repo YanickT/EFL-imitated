baf5e7346889fdb5d943405a394d4a097632a606
Yanick T <50019628+YanickT@users.noreply.github.com>
2021-09-13 11:29:39 +0200

    remove dummy code element

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ gen = DataGenerator(size, 2, mass=0., c=4., batch_size=7)
 structure = [2**i for i in range(1, int(math.log(size, 2)) + 1)][::-1]
 network = Network(structure, c=4.0, flavor=2)
 
-energys = []
 for i, batch in zip(range(40), gen):
     loss = network.train_it(*batch, its=16)
     print(network.J)
